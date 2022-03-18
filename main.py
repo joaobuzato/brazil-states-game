25850b5a82a354ca9ceba6784cc8ced72b70b5a3
Buzato <joaobuzato@gmail.com>
2022-03-18 09:34:52 -0300

    finishing states game

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import turtle
+import pandas
+import unicodedata
+
+
+def clear_string(string):
+    clean_string = ''.join(ch for ch in unicodedata.normalize('NFKD', string) if not unicodedata.combining(ch))
+    return clean_string
 
 
 screen = turtle.Screen()
@@ -8,4 +15,24 @@ screen.addshape(image)
 screen.setup(1000, 1000)
 turtle.shape(image)
 
+data = pandas.read_csv("27_states.csv")
+all_states = data.state.to_list()
+print(all_states)
+
+
+guessed_states = []
+while len(guessed_states) < 27:
+    guess = screen.textinput(title=f"{len(guessed_states)}/27 States Correct", prompt="What's another state?").title()
+    answer_state = clear_string(guess)
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.penup()
+        t.hideturtle()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
 turtle.mainloop()
+
+
+
