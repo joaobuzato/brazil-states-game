7ab29b198c500c6df0723891c6dcd5853e0d7255
Buzato <joaobuzato@gmail.com>
2022-03-18 08:33:08 -0300

    setup do brazil states game

---
+++ main.py
@@ -0,0 +1,11 @@
+import turtle
+
+
+screen = turtle.Screen()
+screen.title("Brazil States Game")
+image = "brazil-states.gif"
+screen.addshape(image)
+screen.setup(1000, 1000)
+turtle.shape(image)
+
+turtle.mainloop()
